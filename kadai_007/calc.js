8271e29457c0e78975dea0a13c1b2b0f29593ede
yaaa-2525 <tianxie.835@gmail.com>
2025-04-20 18:02:12 +0900

    Add files via upload

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,13 +1,16 @@
 let num = Math.floor(Math.random() * 100) + 1; // 1から100までのランダムな整数を代入
 console.log(num);
-if (num % 3 == 0) {
+if (num % 3 == 0 && num % 5 == 0) {
+  console.log("3と5の倍数です");    
+}
+else if (num % 3 == 0) {
   console.log("3の倍数です");
 }
 else if (num % 5 == 0){
   console.log("5の倍数です");
 }
-else if (num % 3 == 0 && num % 5 == 0) {
-  console.log("3と5の倍数です");    
+else {
+  console.log(num);
 }
 
 
