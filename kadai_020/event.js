2a514f56bd19cfc4dd0349f096f72a43b4bcf60b
yaaa-2525 <tianxie.835@gmail.com>
2025-04-27 22:10:06 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,7 @@
+// output-btnというidを持つHTML要素を取得し、定数に代入する
+const btn = document.getElementById('btn');
+
+// HTML要素がクリックされたときにイベント処理を実行する
+btn.addEventListener('click', () => {
+  console.log('ボタンをクリックしました')
+});
